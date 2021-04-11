05577e33a30713d1afa44b7b70768e41e4beb446
AlexRed84 <laleandro@gmail.com>
2021-04-11 18:56:53 +0200

    calificacion añadida

--- front/viajes/src/pages/Rutas.js
+++ front/viajes/src/pages/Rutas.js
@@ -25,6 +25,24 @@ const App = () => {
                   ) : null}
                   {/* <h2 className="h2caja">{item.votes}</h2> */}
                   <h5 className="h5caja">{item.date} </h5>
+                  <div class="ec-stars-wrapper">
+                    <a href="#" data-value="1" title="Votar con 1 estrellas">
+                      &#9733;
+                    </a>
+                    <a href="#" data-value="2" title="Votar con 2 estrellas">
+                      &#9733;
+                    </a>
+                    <a href="#" data-value="3" title="Votar con 3 estrellas">
+                      &#9733;
+                    </a>
+                    <a href="#" data-value="4" title="Votar con 4 estrellas">
+                      &#9733;
+                    </a>
+                    <a href="#" data-value="5" title="Votar con 5 estrellas">
+                      &#9733;
+                    </a>
+                  </div>
+                  <noscript>Necesitas tener habilitado javascript para poder votar</noscript>
                 </div>
               </>
             );
